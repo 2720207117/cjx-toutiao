04b095339d6860b5fcd2a57c2565211b35cd9488
CJX2720207117 <2720207117@qq.com>
2019-07-11 00:29:01 +0800

    登录模块-基础布局和绘制表单

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,29 @@
 <template>
   <div class="login-container">
     <el-card class="box-card">
-        <img src="../../assets/images/logo_index.png" alt="logo_index.png">
-        <!-- 表单 -->
+      <img src="../../assets/images/logo_index.png" alt="logo_index.png" />
 
+      <!-- 表单 -->
+      <el-form>
+        <!-- 表单组件 -->
+        <el-form-item>
+          <!-- 表单项组件 -->
+          <el-input placeholder="请输入手机号"></el-input>
+          <!-- 表单元素 -->
+        </el-form-item>
+        <el-form-item>
+          <!-- 表单项组件 -->
+          <el-input placeholder="验证码" style="width: 280px;"></el-input>
+          <!-- 表单元素 -->
+          <el-button style="float: right;">发送验证</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="checked"></el-checkbox> 我已阅读并同意<el-link type="primary">用户协议</el-link>和<el-link type="primary">隐私条款</el-link>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary" style="width: 100%">登录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
@@ -11,7 +31,14 @@
 <script>
 export default {
   // 导出
-
+  data () {
+    return {
+      form: {
+        name: ''
+      },
+      checked: true
+    }
+  }
 }
 </script>
 
@@ -34,9 +61,15 @@ export default {
   top: 50%;
   transform: translate(-50%, -50%);
   img {
-      display: block;
-      width: 200px;
-      margin: 0 auto;
+    display: block;
+    width: 200px;
+    margin: 10px auto 30px;
+  }
+  a {
+      text-decoration: none;
+  }
+  .el-checkbox {
+      margin-right: 5px;
   }
 }
 </style>
