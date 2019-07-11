9890aa9fa79ad5e1a4e914a50b0df40973a32dee
CJX2720207117 <2720207117@qq.com>
2019-07-11 16:41:26 +0800

    登录模块-单个元素校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,19 +4,19 @@
         <img src="../../assets/images/logo_index.png" alt="logo_index.png">
 
         <!-- 表单 -->
-         <el-form>  <!-- 表单组件 -->
-        <el-form-item>  <!-- 表单项组件 -->
-          <el-input placeholder="请输入手机号"></el-input>  <!-- 表单元素 -->
+      <el-form :model="loginForm" :rules="loginRules">  <!-- 表单组件 -->
+        <el-form-item prop="mobile">  <!-- 表单项组件 -->
+          <el-input placeholder="请输入手机号" v-model="loginForm.mobile"></el-input>  <!-- 表单元素 -->
         </el-form-item>
-        <el-form-item>  <!-- 表单项组件 -->
-          <el-input placeholder="验证码" style="width: 280px;"></el-input>  <!-- 表单元素 -->
+        <el-form-item prop="code">  <!-- 表单项组件 -->
+          <el-input placeholder="验证码" style="width: 280px;" v-model="loginForm.code"></el-input>  <!-- 表单元素 -->
           <el-button style="float: right;">发送验证</el-button>
         </el-form-item>
         <el-form-item>
           <el-checkbox v-model="checked"></el-checkbox> 我已阅读并同意<el-link type="primary">用户协议</el-link>和<el-link type="primary">隐私条款</el-link>
         </el-form-item>
         <el-form-item>
-            <el-button type="primary" style="width: 100%">登录</el-button>
+          <el-button type="primary" style="width: 100%">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -28,10 +28,26 @@ export default {
   // 导出
   data () {
     return {
-      form: {
-        name: ''
+      // 表单对应的对象
+      loginForm: {
+        mobile: '',
+        code: ''
       },
+      // 表单的校验规则对象
+      loginRules: {
+        mobile: [
+          // 具体的校验规则 例: 是否必填 长度 格式
+          { required: true, message: '手机号必填', trigger: 'blur' },
+          { max: 11, min: 11, message: '手机号为11位数字', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '验证码必填', trigger: 'blur' }
+        ]
+      },
+
+      // 默认选中复选框
       checked: true
+
     }
   }
 }
