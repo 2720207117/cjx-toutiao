9e984132d4e61fbe3ba752e3cb96838e2eb61df6
CJX2720207117 <2720207117@qq.com>
2019-07-12 15:31:52 +0800

    首页模块-路由与组件

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@
 import VueRouter from 'vue-router' // 导入vue-router
 import Vue from 'vue' // 注册vueRouter需要Vue对象   先导入Vue
 import Login from '@/views/login' // 导入login组件
+import Home from '@/views/home' // 导入home组件
 Vue.use(VueRouter) // 注册vueRouter
 
 const router = new VueRouter({ // 初始化router对象  (配置路由规则)
   routes: [
-    { name: 'login', path: '/login', component: Login }
+    { name: 'login', path: '/login', component: Login },
+    { name: 'home', path: '/', component: Home }
   ]
 })
 
